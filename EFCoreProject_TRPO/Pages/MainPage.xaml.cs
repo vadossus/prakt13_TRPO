0b6862fa90f25fab9386b5e5d2b181313457efc4
vadossus <vadim.gorowoy2015@gmail.com>
2025-12-04 08:48:27 +0500

    Update: 3.0

--- EFCoreProject_TRPO/Pages/MainPage.xaml.cs
+++ EFCoreProject_TRPO/Pages/MainPage.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Globalization;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace EFCoreProject_TRPO.Pages
 {
@@ -11,19 +11,74 @@ namespace EFCoreProject_TRPO.Pages
         public UsersService service { get; set; } = new();
         public User? selectedUser { get; set; } = null;
 
+        public ObservableCollection<Role> Roles { get; set; } = new();
+
+
+        private int? _selectedRoleId = null;
+        public int? SelectedRoleId
+        {
+            get => _selectedRoleId;
+            set
+            {
+                _selectedRoleId = value;
+                UpdateFUsers();
+            }
+        }
+
+        public ObservableCollection<User> FUsers { get; set; } = new();
+
         public MainPage()
         {
             InitializeComponent();
+
+            LoadRoles();
+
             DataContext = this;
             this.Loaded += MainPage_Loaded;
+        }
+
+        private void LoadRoles()
+        {
+            var roleService = new RoleService();
+            Roles.Clear();
+
+            Roles.Add(new Role { Id = 0, Title = "Все роли" });
+
+            foreach (var role in roleService.Roles)
+            {
+                Roles.Add(role);
+            }
+
+            SelectedRoleId = 0;
         }
 
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
             service.GetAll();
+            UpdateFUsers();
 
             DataContext = null;
             DataContext = this;
+        }
+
+        private void UpdateFUsers()
+        {
+            FUsers.Clear();
+
+            if (SelectedRoleId == null || SelectedRoleId == 0)
+            {
+                foreach (var user in service.Users)
+                {
+                    FUsers.Add(user);
+                }
+            }
+            else
+            {
+                foreach (var user in service.Users.Where(u => u.RoleId == SelectedRoleId))
+                {
+                    FUsers.Add(user);
+                }
+            }
         }
 
         public void go_form(object sender, RoutedEventArgs e)
@@ -53,7 +108,19 @@ namespace EFCoreProject_TRPO.Pages
                 MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
             {
                 service.Remove(selectedUser);
+                UpdateFUsers(); 
             }
+        }
+
+        public void ClearFilter(object sender, RoutedEventArgs e)
+        {
+            SelectedRoleId = 0;
+        }
+
+        public void RefreshUsers()
+        {
+            service.GetAll();
+            UpdateFUsers();
         }
     }
 }
